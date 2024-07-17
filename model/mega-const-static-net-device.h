c6b09b0a9618784783b0c0cccfbcd09d47a7035b
marbu <martin.anschuetz@vert-tec.de>
2024-07-17 20:21:03 +0000

    eod

--- model/mega-const-static-net-device.h
+++ model/mega-const-static-net-device.h
@@ -13,17 +13,23 @@ public:
     
     static TypeId GetTypeId();
 
+    
     MegaConstellationStaticNetDevice();
 
+    
     void Receive(Ptr<Packet> packet, Mac48Address src, Mac48Address dst);
 
-    void SetChannelAttribute(Ptr<Channel> channel);
+    void SetChannel(Ptr<Channel> channel);
 
     void SetAddress(Mac48Address address);
 
 private:
-
+    /**
+     * To keep compatability with IP Stack
+     */
     Mac48Address m_address;
+    
+    Ptr<Channel> m_channel;
 
 
 };  /* MegaConstellationStaticNetDevice */
